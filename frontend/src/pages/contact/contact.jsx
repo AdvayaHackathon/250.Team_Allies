6cd27af30f4c27dacf05adadae87a220af3870bd
charan km <charanghost7@gmail.com>
2025-04-12 07:14:34 +0530

    updated footer

--- frontend/src/pages/contact/contact.jsx
+++ frontend/src/pages/contact/contact.jsx
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
-//import '../styles/Pages.css';
-
-const ContactUs = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-
-  const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Message Sent", formData);
-    alert('Message sent!');
-  };
-
+import React from 'react';
+import './contact.css';
+const ContactUs = () => { 
   return (
     <div className="page-container">
       <h1>Contact Us</h1>
-      <form className="contact-form" onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Your Name" onChange={handleChange} required />
-        <input type="email" name="email" placeholder="Your Email" onChange={handleChange} required />
-        <textarea name="message" placeholder="Your Message" rows="5" onChange={handleChange} required />
-        <button type="submit">Send Message</button>
-      </form>
+     <p>9999888000</p>
+     <p>healthriskassessment@gmail.com </p>
     </div>
   );
 };
-
 export default ContactUs;
